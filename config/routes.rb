b903bb33e85c0c555caae3dd74b120b5c2737540
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-16 21:34:04 +0000

    added name to services

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :neighbourhoods, only: [:index, :show] do
+    resources :services do
+      resources :reviews, only: [:new, :create, :edit, :destroy]
+      resources :favourites, only: [:create, :destroy]
+    end
+    resources :news, only: [:index]
+    resources :posts
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,73 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_152829) do
+ActiveRecord::Schema.define(version: 2020_11_16_205225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "service_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["service_id"], name: "index_favourites_on_service_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "neighbourhoods", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "news", force: :cascade do |t|
+    t.text "content"
+    t.bigint "neighbourhood_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["neighbourhood_id"], name: "index_news_on_neighbourhood_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "neighbourhood_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["neighbourhood_id"], name: "index_posts_on_neighbourhood_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "service_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["service_id"], name: "index_reviews_on_service_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "address"
+    t.integer "phone"
+    t.time "time"
+    t.string "socialmedia"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "neighbourhood_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["neighbourhood_id"], name: "index_services_on_neighbourhood_id"
+    t.index ["user_id"], name: "index_services_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +86,19 @@ ActiveRecord::Schema.define(version: 2020_11_16_152829) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "serviceowner"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "services"
+  add_foreign_key "favourites", "users"
+  add_foreign_key "news", "neighbourhoods"
+  add_foreign_key "posts", "neighbourhoods"
+  add_foreign_key "posts", "users"
+  add_foreign_key "reviews", "services"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "services", "neighbourhoods"
+  add_foreign_key "services", "users"
 end
