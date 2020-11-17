3d52fd82a1a1f61e65740525098dcd0aa6396990
luis domingues <dominguesfluis@gmail.com>
2020-11-17 15:26:58 +0000

    fazer review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   def new
+
   end
 
   def create
@@ -11,3 +12,32 @@ class ReviewsController < ApplicationController
   def destroy
   end
 end
+
+
+def new
+    @favourite = Favourite.new
+  end
+
+  def create
+    @service = Service.find(params[:service_id])
+    @favourite = Favourite.new(favourite_params)
+    @favourite.service = @service
+    @favourite.user = current_user
+    if @favourite.save
+      redirect_to service_path(@service)
+    else
+      render '???'
+    end
+  end
+
+  def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
+    redirect_to service_path(@service.service)
+  end
+
+  private
+
+  def favourite_params
+    params.permit(:service_id)
+  end
