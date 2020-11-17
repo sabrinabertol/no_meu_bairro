43a5e648078f6b686ad9237681a767944c3cebd0
romulo nesi <romulonesi@gmail.com>
2020-11-17 15:32:01 +0000

    Amost completed user edit name, password, photo and bairro

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+
   resources :neighbourhoods, only: [:index, :show] do
     resources :services do
       resources :reviews, only: [:new, :create, :edit, :destroy]
@@ -10,3 +11,4 @@ Rails.application.routes.draw do
     resources :news, only: [:index]
     resources :posts
   end
+end
