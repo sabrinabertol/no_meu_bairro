7859aa41da23f2e10f06214730a2a5f7492601cd
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-19 17:26:32 +0000

    front-end things

--- app/models/neighbourhood.rb
+++ app/models/neighbourhood.rb
@@ -3,4 +3,10 @@ class Neighbourhood < ApplicationRecord
   has_many :users
   has_many :news
   has_many :posts
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
