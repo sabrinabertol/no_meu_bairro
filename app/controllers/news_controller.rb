534552020e5e20c9414f2b5ef5492f76103f305e
Luis Domingues <dominguesfluis@gmail.com>
2020-11-21 17:53:19 +0000

    news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,22 @@
 class NewsController < ApplicationController
-  def index
+  before_action :set_neighbourhood
+
+  def new
+    @news = New.new
+  end
+
+  def create
+      @new= New.new(news_params)
+      @new.neighbourhood = @neighbourhood
+      @new.save
+  end
+
+  private
+
+  def news_params
+    params.require(:new).permit(:url, :title, :content)
+
+  def set_neighbourhood
+    @neighbourhood = Neighbourhood.find(params[:neighbourhood_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_132602) do
+ActiveRecord::Schema.define(version: 2020_11_21_171730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 2020_11_20_132602) do
     t.bigint "neighbourhood_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "url"
     t.index ["neighbourhood_id"], name: "index_news_on_neighbourhood_id"
   end
 
