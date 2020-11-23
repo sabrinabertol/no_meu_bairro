6cb3d0ae5cee93337fe77fcebe375b4b74060bd9
Luis Domingues <dominguesfluis@gmail.com>
2020-11-23 16:04:17 +0000

    news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,20 +1,9 @@
 class NewsController < ApplicationController
   before_action :set_neighbourhood
 
-  def new
-    @news = New.new
+  def index
+    @news = NewsService.call
   end
-
-  def create
-      @new= New.new(news_params)
-      @new.neighbourhood = @neighbourhood
-      @new.save
-  end
-
-  private
-
-  def news_params
-    params.require(:new).permit(:url, :title, :content)
 
   def set_neighbourhood
     @neighbourhood = Neighbourhood.find(params[:neighbourhood_id])
