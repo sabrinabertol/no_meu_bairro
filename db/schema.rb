8e833391d95cb842a1259c932d89e93219ed4e2a
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-23 10:09:37 +0000

    weekend changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_132602) do
+ActiveRecord::Schema.define(version: 2020_11_21_165956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_132602) do
     t.datetime "weekdays"
     t.string "category"
     t.text "description"
+    t.boolean "favourite", default: false
     t.index ["neighbourhood_id"], name: "index_services_on_neighbourhood_id"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
