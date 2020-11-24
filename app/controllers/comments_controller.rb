d0466a9d3553a85972086abeffb897f447d0dd7a
romulo nesi <romulonesi@gmail.com>
2020-11-24 16:06:06 +0000

    scss beggining cards of forum

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:comment))
-    comment.user_id = current_user.id if current_user
+    @comment.user_id = current_user.id if current_user
     @comment.save
 
     if @comment.save
