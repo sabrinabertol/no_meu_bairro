1ef5f46add5837ed99da7c962bf1d62e5bd27eb3
ruben serrano <rubserr@outlook.com>
2020-11-24 15:46:07 +0000

    no chantes just a commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :reviews
   has_many :favourites
-  has_many :comments
   has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
 
   def has_favourited?(service)
     favourites.find_by(service_id:service.id).present?
