0a19572b034d2446ce05452ae480a683d69b8756
romulo nesi <romulonesi@gmail.com>
2020-11-25 19:09:25 +0000

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,20 @@
 Rails.application.routes.draw do
-
   devise_for :users
   get "/dashboard", to: "pages#dashboard"
   get "/about", to: "pages#about"
   get "/favourites", to: "pages#favourites", as: "favourites"
-
   root to: 'neighbourhoods#index'
   resources :news, only: [:index ]
-
   resources :neighbourhoods, only: [:index, :show] do
+      resources :posts do
+        resources :comments, except: [:destroy]
+      end
     resources :services do
       resources :reviews, only: [:new, :create, :edit, :destroy]
       member do
         post "fav", to: "services#fav"
         post "unfav", to: "services#unfav"
       end
-      resources :posts do
-      resources :comments, except: [:destroy]
-    end
-      # resources :favourites, only: [:create, :destroy]
     end
   end
   resources :posts, only: [] do
