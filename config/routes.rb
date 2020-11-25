fbf2c316d14fde346c5c04de17fc58601fa75be5
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-25 18:05:06 +0000

    fixed button search

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users 
+  devise_for :users
   get "/dashboard", to: "pages#dashboard"
   get "/about", to: "pages#about"
-  
+
   root to: 'neighbourhoods#index'
   resources :news, only: [:index ]
   resources :neighbourhoods, only: [:index, :show] do
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
         post "fav", to: "services#fav"
         post "unfav", to: "services#unfav"
       end
-
-      # resources :favourites, only: [:create, :destroy]
-    end
-    resources :posts do
+      resources :posts do
       resources :comments, except: [:destroy]
     end
+      # resources :favourites, only: [:create, :destroy]
+    end
+
   end
   resources :posts, only: [] do
     resources :comments, only: [:destroy]
