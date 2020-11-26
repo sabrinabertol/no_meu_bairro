ab8f436362504cbbd4522e5da0ca82d4af5227aa
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-26 18:17:06 +0000

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,8 @@ def create_users()
 
   (1..20).each do |id|
     User.create!(
-      name:Faker::FunnyName.two_word_name, 
-      password: "123456", 
+      name:Faker::FunnyName.two_word_name,
+      password: "123456",
       email: Faker::Internet.email,
       neighbourhood: Neighbourhood.all.sample,
       )
@@ -65,9 +65,9 @@ def create_services(bairro, name_ok)
 
     postal_code = "#{service[:COD_POST_4]}-#{service[:COD_POST_3]}"
 
-    if bairro.include? postal_code 
-      
-      
+    if bairro.include? postal_code
+
+
       categories = ['business','food','city','people','transport']
       url = "http://lorempixel.com/1024/600/#{categories.sample}/#{rand(1..10)}"
       file = URI.open(url)
@@ -85,9 +85,9 @@ def create_services(bairro, name_ok)
                             user: User.first
 
                             )
-                            
+
                             service.photos.attach(io: file, filename: "#{categories.sample}.png", content_type: 'image/png')
-      create_reviews()  
+      create_reviews()
     end
   end
   puts " services OK"
@@ -107,8 +107,8 @@ def create_reviews()
   puts "reviews ok"
 end
 
-# acept a string with format for nokogiri 
-# and the correct name for neighboord create 
+# acept a string with format for nokogiri
+# and the correct name for neighboord create
 def o_meu_bairro(o_meu_bairro, name_ok)
   puts "a obter os codigos postais do bairro #{name_ok} "
 
@@ -117,26 +117,26 @@ def o_meu_bairro(o_meu_bairro, name_ok)
   html_doc = Nokogiri::HTML(Net::HTTP.get(URI(url)))
   cp_list = []
 
- 
+
 
   html_doc.search('.list-unstyled.long-list').each do |element|
     element.children.css('a').each do |cp|
       cp_list << cp.text[0..7]
      end
   end
-  
+
   puts "codigos postais do #{o_meu_bairro} ok ! a criar serviços e reviews"
 
   create_services(cp_list, name_ok)
-  
+
 end
 
 
 destroy()
 create_neighbourhood()
 create_users()
-# + Santo António 
-o_meu_bairro("santo-antonio","Santo António")
+# + Santo António
+# o_meu_bairro("santo-antonio","Santo António")
 # + Parque das Nações
 o_meu_bairro("parque-das-nacoes","Parque das Nações")
 # + Marvila
@@ -144,49 +144,49 @@ o_meu_bairro("marvila","Marvila")
 # + Ajuda
 o_meu_bairro("ajuda","Ajuda")
 # + Areeiro
-o_meu_bairro("areeiro","Areeiro")
-# + Santa Maria Maior
-o_meu_bairro("santa-maria-maior","Santa Maria Maior")
+# o_meu_bairro("areeiro","Areeiro")
+# # + Santa Maria Maior
+# o_meu_bairro("santa-maria-maior","Santa Maria Maior")
 # + Alvalade
 o_meu_bairro("alvalade","Alvalade")
 # + Belém
 o_meu_bairro("belem","Belém")
 # + Estrela
-o_meu_bairro("estrela","Estrela")
-# + Arroios
-o_meu_bairro("arroios","Arroios")
-# + Santa Clara
-o_meu_bairro("santa-clara","Santa Clara")
-# + Avenidas Novas
-o_meu_bairro("avenidas-novas","Avenidas Novas")
-# + Carnide
-o_meu_bairro("carnide","Carnide")
-# + São Domingos de Benfica
-o_meu_bairro("sao-domingos-de-benfica","São Domingos de Benfica")
-# + Beato
-o_meu_bairro("beato","Beato")
+# o_meu_bairro("estrela","Estrela")
+# # + Arroios
+# o_meu_bairro("arroios","Arroios")
+# # + Santa Clara
+# o_meu_bairro("santa-clara","Santa Clara")
+# # + Avenidas Novas
+# o_meu_bairro("avenidas-novas","Avenidas Novas")
+# # + Carnide
+# o_meu_bairro("carnide","Carnide")
+# # + São Domingos de Benfica
+# o_meu_bairro("sao-domingos-de-benfica","São Domingos de Benfica")
+# # + Beato
+# o_meu_bairro("beato","Beato")
 # + Campolide
 o_meu_bairro("campolide","Campolide")
 # + Alcântara
-o_meu_bairro("alcantara","Alcântara")
-# + Campo de Ourique
-o_meu_bairro("campo-de-ourique","Campo de Ourique")
-# + São Vicente
-o_meu_bairro("sao-vicente","São Vicente")
-# + Olivais
-o_meu_bairro("olivais","Olivais")
-# + Misericórdia
-o_meu_bairro("misericordia","Misericórdia")
-# + Lumiar
-o_meu_bairro("lumiar","Lumiar")
-# + Penha de França
-o_meu_bairro("penha-de-franca","Penha de França")
-# + Benfica
-o_meu_bairro("benfica","Benfica")
+# o_meu_bairro("alcantara","Alcântara")
+# # + Campo de Ourique
+# o_meu_bairro("campo-de-ourique","Campo de Ourique")
+# # + São Vicente
+# o_meu_bairro("sao-vicente","São Vicente")
+# # + Olivais
+# o_meu_bairro("olivais","Olivais")
+# # + Misericórdia
+# o_meu_bairro("misericordia","Misericórdia")
+# # + Lumiar
+# o_meu_bairro("lumiar","Lumiar")
+# # + Penha de França
+# o_meu_bairro("penha-de-franca","Penha de França")
+# # + Benfica
+# o_meu_bairro("benfica","Benfica")
 
-o_meu_bairro("santo-antonio", "Santo António")
+# o_meu_bairro("santo-antonio", "Santo António")
 
-o_meu_bairro("parque-das-nacoes", "Parque das Nações")
+# o_meu_bairro("parque-das-nacoes", "Parque das Nações")
 
-o_meu_bairro("areeiro", "Areeiro")
+# o_meu_bairro("areeiro", "Areeiro")
 
